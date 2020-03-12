023b1eb1bac07964664dbf27eefcfa45e2ab7cf5
Zac Smith <zrs3141592@gmail.com>
2020-03-11 20:10:39 -0600

    Node Callbacks and Error Handling

--- index.js
+++ index.js
@@ -1,19 +1,46 @@
 const rect = require("./rectangle.js");
 
-function solveRect(l, w) {
-  console.log(`Solving for rectange with l ${l} and width ${w}`);
-
-  if (l <= 0 || w <= 0) {
-    console.log(
-      `Length and/or width should be greater than zero where l= ${l} and w=${w}`
-    );
-  } else {
-    console.log(`the area is ${rect.area(l, w)}`);
-    console.log(`the peremeter is ${rect.perimeter(l, w)}`);
-  }
-
-  // (l <= 0 || w <= 0) ? console.log(`sizes should be greater than zero where l= ${l} and w=${w}`) : console.log(`is good`)
+function solveRect(l, b) {
+  console.log("Solving for rectangle with l = " + l + " and b = " + b);
+  rect(l, b, (err, rectangle) => {
+    if (err) {
+      console.log("ERROR: ", err.message);
+    } else {
+      console.log(
+        "The area of the rectangle of dimensions l = " +
+          l +
+          " and b = " +
+          b +
+          " is " +
+          rectangle.area()
+      );
+      console.log(
+        "The perimeter of the rectangle of dimensions l = " +
+          l +
+          " and b = " +
+          b +
+          " is " +
+          rectangle.perimeter()
+      );
+    }
+  });
+  console.log("This statement after the call to rect()");
 }
+
+// function solveRect(l, w) {
+//   console.log(`Solving for rectange with l ${l} and width ${w}`);
+
+//   rect(l, w, (err, rectangle) => {
+//     if (err) {
+//       console.log("Error", err.message);
+//     } else {
+//       console.log(`The area of ${l} and ${b} is ${rectangle.area()}`);
+//       console.log(`The area of ${l} and ${b} is ${rectangle.perimeter()}`);
+//     }
+//   });
+
+//   // (l <= 0 || w <= 0) ? console.log(`sizes should be greater than zero where l= ${l} and w=${w}`) : console.log(`is good`)
+// }
 
 solveRect(2, 3);
 solveRect(3, 5);
--- rectangle.js
+++ rectangle.js
@@ -1,14 +1,40 @@
+// module.exports = (x, y, callback) => {
+//   if (x <= 0 || y <= 0) {
+//     setTimeout(() =>
+//       callback (new Error(`Length and/or width should be greater than zero where l= ${x} and w=${y}`), null),
+//     2000)
+
+// } else {
+//   setTimeout(() =>
+//     callback (null, {
+//       perimeter = () => (2*(x+y)),
+//       area = () => (x*y)
+//     }),
+//   2000)
+// }
+
 module.exports = (x, y, callback) => {
-  if (x <= 0 || y <= 0) {
-    setTimeout(() => 
-      callback (new Error(`Length and/or width should be greater than zero where l= ${x} and w=${y}`), null),
-    2000)
-    
-} else {
-  setTimeout(() => 
-    callback (null, {
-      perimeter = (x,y) => (2*(x+y)),
-      area = (x,y) => (x*y)
-    }),
-  2000)
-}
+  if (x <= 0 || y <= 0)
+    setTimeout(
+      () =>
+        callback(
+          new Error(
+            "Rectangle dimensions should be greater than zero: l = " +
+              x +
+              ", and b = " +
+              y
+          ),
+          null
+        ),
+      2000
+    );
+  else
+    setTimeout(
+      () =>
+        callback(null, {
+          perimeter: () => 2 * (x + y),
+          area: () => x * y
+        }),
+      2000
+    );
+};
