ea6288eea42f1fd9f4b682e68683822530cc2050
Zac Smith <zrs3141592@gmail.com>
2020-05-17 22:10:55 -0600

    Express static

--- node-express/index.js
+++ node-express/index.js
@@ -1,16 +1,20 @@
 const express = require("express");
 const http = require("http");
+const morgan = require("morgan");
 
 const hostname = "localhost";
 const port = 3000;
 
 const app = express();
+app.use(morgan("dev"));
+
+app.use(express.static(__dirname + "/public"));
 
 app.use((req, res, next) => {
   console.log(req.headers);
   res.statusCode = 200;
   res.setHeader("Context-Type", "text/html");
-  res.end("<html><body><h1>This is an express server</h1></body></html>");
+  res.send({ message: "You are using Express" });
 });
 
 const server = http.createServer(app);
