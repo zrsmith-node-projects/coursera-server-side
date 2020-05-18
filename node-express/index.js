affaab8724df8084b971938481a4d51346a923e1
Zac Smith <zrs3141592@gmail.com>
2020-05-17 22:44:12 -0600

    simple express example

--- node-express/index.js
+++ node-express/index.js
@@ -1,12 +1,65 @@
 const express = require("express");
 const http = require("http");
 const morgan = require("morgan");
+const bodyParser = require("body-parser");
 
 const hostname = "localhost";
 const port = 3000;
 
 const app = express();
 app.use(morgan("dev"));
+app.use(bodyParser.json());
+
+app.all("/dishes", (req, res, next) => {
+  res.statusCode = 200;
+  res.setHeader("Context-Type", "text/plain");
+  next();
+});
+
+app.get("/dishes", (req, res) => {
+  res.end("Will send all the dishes to you!");
+});
+
+app.post("/dishes", (req, res) => {
+  res.end(
+    "Will add the dish: " +
+      req.body.name +
+      " with details: " +
+      req.body.description
+  );
+});
+
+app.put("/dishes", (req, res) => {
+  res.statusCode = 403;
+  res.end("PUT operation not supported on /dishes");
+});
+
+app.delete("/dishes", (req, res) => {
+  res.end("Deleting all the dishes");
+});
+
+app.get("/dishes/:id", (req, res) => {
+  res.end("Will send details of the dish: " + req.params.id);
+});
+
+app.post("/dishes/:id", (req, res) => {
+  res.statusCode = 403;
+  res.end("POST operation not supported on /dishes/" + req.params.id);
+});
+
+app.put("/dishes/:id", (req, res) => {
+  res.write("updating dish " + req.params.id + "\n");
+  res.end(
+    "Will update the dish: " +
+      req.body.name +
+      " with details " +
+      req.body.description
+  );
+});
+
+app.delete("/dishes/:id", (req, res) => {
+  res.end("Deleting dish: " + req.params.id);
+});
 
 app.use(express.static(__dirname + "/public"));
 
