d5c06a8cf96e53dee8bf638e95914e85f1cd3da1
Zac Smith <zrs3141592@gmail.com>
2020-05-23 20:59:07 -0600

    node mongo

--- node-mongo/index.js
+++ node-mongo/index.js
@@ -1,5 +1,6 @@
 const MongoClient = require("mongodb").MongoClient;
 const assert = require("assert");
+const dboper = require("./operations");
 
 const URL = "mongodb://localhost:27017/";
 const dbname = "conFusion";
@@ -9,26 +10,55 @@ MongoClient.connect(URL, (err, client) => {
   console.log("Connected correctly to server");
 
   const db = client.db(dbname);
-  const collection = db.collection("dishes");
-  collection.insertOne(
-    { name: "pizza", description: "testing pizza" },
-    (err, result) => {
-      assert.equal(err, null);
-      console.log("After insert");
-      console.log(result.ops);
 
-      collection.find({}).toArray((err, docs) => {
-        assert.equal(err, null);
-
-        console.log("Found: \n");
-        console.log(docs);
-
-        db.dropCollection("dishes", (err, result) => {
-          assert.equal(err, null);
-
-          client.close();
-        });
+  dboper.insertDocument(
+    db,
+    { name: "Vadonut", description: "TEST" },
+    "dishes",
+    (result) => {
+      console.log(("insert document: \n", result.ops));
+      dboper.findDocuments(db, "dishes", (docs) => {
+        console.log("Found documents:\n ", docs);
+        dboper.updateDocument(
+          db,
+          { name: "Vadonut" },
+          { description: "Updated Test" },
+          "dishes",
+          (result) => {
+            console.log("Update document:\n", result.result);
+            dboper.findDocuments(db, "dishes", (docs) => {
+              console.log("found updated documents:\n", docs);
+              db.dropCollection("dishes", (result) => {
+                console.log("Dropped collection");
+                client.close();
+              });
+            });
+          }
+        );
       });
     }
   );
 });
+
+// const collection = db.collection("dishes");
+//   collection.insertOne(
+//     { name: "pizza", description: "testing pizza" },
+//     (err, result) => {
+//       assert.equal(err, null);
+//       console.log("After insert");
+//       console.log(result.ops);
+
+//       collection.find({}).toArray((err, docs) => {
+//         assert.equal(err, null);
+
+//         console.log("Found: \n");
+//         console.log(docs);
+
+//         db.dropCollection("dishes", (err, result) => {
+//           assert.equal(err, null);
+
+//           client.close();
+//         });
+//       });
+//     }
+//   );
